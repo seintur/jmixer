6b54b203f1a39e3354531d77cacf2a6653361ef3
Lionel Seinturier <Lionel.Seinturier@univ-lille1.fr>
2014-09-28 13:42:37 +0200

    Fix test under maven.

--- src/test/java/jmixer/MixinClassGeneratorTest.java
+++ src/test/java/jmixer/MixinClassGeneratorTest.java
@@ -1,20 +1,31 @@
 package jmixer;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.junit.Test;
 
 public class MixinClassGeneratorTest {
 
 	@Test
 	public void testMixer() throws Exception {
+		
+		String classpath = System.getProperty("java.class.path");
 		String[] args =
 			new String[] {
 				"-i", "src/test/java",
-				"-d", "target/classes",
+				"--source-classpath", classpath,
+				"-o", "target/spooned",
+				"-d", "target/spooned-classes",
 				"--processors", MixinProcessor.class.getName(),
 				"--compile" };
 		spoon.Launcher.main(args);
 		
-		Duck.class.getMethod("fly");
-		Duck.class.getMethod("swim");
+		URLClassLoader urlcl =
+			new URLClassLoader(new URL[]{new URL("file:target/spooned-classes/")},null);
+		
+		urlcl.loadClass("jmixer.Duck").getMethod("fly");
+		urlcl.loadClass("jmixer.Duck").getMethod("swim");
+		urlcl.close();
 	}
 }
